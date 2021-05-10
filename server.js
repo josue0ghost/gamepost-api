d73a353d33ba11b44838acbc8f5cc51e4a41e799
Emmanuel Alvarado <josue0ghost@gmail.com>
2021-05-10 13:39:32 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 
-var express = require('express'),
-var cors = require('cors'),
-app = express(),
+var express = require('express')
+var cors = require('cors')
+app = express()
 port = process.env.PORT || 3000;
 
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-app.use(cors())
+const corsOpts = {
+    origin: '*',
+  
+    methods: [
+      'GET',
+      'POST',
+    ],
+  
+    allowedHeaders: [
+      'Content-Type',
+    ],
+  };
+app.use(cors(corsOpts))
 
 var routes = require('./api/routes/index');
 module.exports = app;
